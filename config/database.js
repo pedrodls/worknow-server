a4e288e41b5bfd839c365cce2d7d3594e87af9ea
pedro joão <66138071+pedrojoao-dev@users.noreply.github.com>
2021-10-15 13:40:23 +0100

    Database configuration done

---
+++ config/database.js
@@ -0,0 +1,18 @@
+
+//Database configuration
+
+module.exports = {
+    dev: {
+        database: "worknowdb",
+        user: "rooter",
+        password: "",
+        host: "localhost",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+}

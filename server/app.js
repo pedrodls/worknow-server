15291499a0a99da83800e2e7b7a37d167119a78f
pedro joão <66138071+pedrojoao-dev@users.noreply.github.com>
2021-10-15 14:34:10 +0100

    Server configuration done

--- server/app.js
+++ server/app.js
@@ -1,1 +1,25 @@
 const express = require("express")
+
+const app = express()
+
+const server = require("http").Server(app)
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.json({extended: true}))
+
+app.use(express.static("/public"))
+
+app.use("/static", express.static("/public"))
+
+app.get("/", (req, res) =>{
+    res.send("| Welcome to worknow API |")
+})
+
+app.set("port", 9800)
+
+app.set("address", "localhost")
+
+server.listen(app.get("port"), app.get("address"), () => {
+    console.log("Server has running at http://"+app.get("address")+":"+app.get("port"))
+})
